bd5d79eddef84469438d298fe1df02d1db55745d
djawed-bkh <bekhouchadjawed@gmail.com>
2017-11-27 00:29:02 +0100

    70%

---
+++ src/sample/Controller.java
@@ -0,0 +1,116 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+import java.util.Random;
+
+public class Controller {
+    // initialisations
+    @FXML
+    private TextField screen1;
+    @FXML
+    private TextField screen;
+    @FXML
+    private TextField screen11;
+    private int p=1;
+    private int f=2;
+    private int c=3;
+    private int val=0;
+    private int scorej=0;
+    private int scoreo=0;
+    private int joue;
+
+
+    String nom;
+
+    int i=0;
+    private int nbrpartie=3;
+    //initialisations
+
+    @FXML
+public void clique(ActionEvent event){
+
+
+    if (i<=nbrpartie){
+       nom=((Button)event.getSource()).getText();
+        System.out.println(nom);
+       joue=Integer.parseInt(nom);
+       calculscore();
+        i++;
+
+    }
+    if (i>nbrpartie){
+        if (scorej>scoreo){
+            screen11.setText("the winner is joueur with "+scorej+" points");
+        }else if(scorej<scoreo){
+            screen11.setText("the winner is ordi with "+scoreo+" points");
+
+        }else {
+            screen11.setText("partie nulle ");
+        }
+    }
+
+
+}
+    //l'ordinateur qui joue
+    public int ordiplay(){
+        int low=1;
+        int high=3;
+        return val=(int)(Math.random()*(high-low))+low;
+    }
+
+    public void calculscore(){
+        if (ordiplay()==1 && joue==2){
+            scorej+=1;
+            scoreo+=0;
+            screen.setText("ordinateur"+scoreo);
+            screen1.setText("joueur"+scorej);
+        }
+        if (ordiplay()==2 && joue==1){
+            scorej+=0;
+            scoreo+=1;
+            screen.setText("ordinateur"+scoreo);
+            screen1.setText("joueur"+scorej);
+        }
+        if (ordiplay()==1 && joue==3){
+            scorej+=0;
+            scoreo+=1;
+            screen.setText("ordinateur"+scoreo);
+            screen1.setText("joueur"+scorej);
+        }
+        if (ordiplay()==3 && joue==1){
+            scorej+=1;
+            scoreo+=0;
+            screen.setText("ordinateur"+scoreo);
+            screen1.setText("joueur"+scorej);
+        }
+        if (ordiplay()==2 && joue==3){
+            scorej+=1;
+            scoreo+=0;
+            screen.setText("ordinateur"+scoreo);
+            screen1.setText("joueur"+scorej);
+        }
+        if (ordiplay()==1 && joue==1){
+            scorej+=0;
+            scoreo+=0;
+            screen.setText("ordinateur"+scoreo);
+            screen1.setText("joueur"+scorej);
+        }
+        if (ordiplay()==2 && joue==2){
+            scorej+=0;
+            scoreo+=0;
+            screen.setText("ordinateur"+scoreo);
+            screen1.setText("joueur"+scorej);
+        }
+        if (ordiplay()==3 && joue==3){
+            scorej+=0;
+            scoreo+=0;
+            screen.setText("ordinateur"+scoreo);
+            screen1.setText("joueur"+scorej);
+        }
+    }
+}
